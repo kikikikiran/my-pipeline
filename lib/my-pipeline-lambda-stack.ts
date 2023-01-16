5982c7dc58dc7dada1d764cf0cb5b9a9db0a825e
devopscarscan <110095251+devopscarscan@users.noreply.github.com>
2023-01-16 16:24:40 +0530

    4th commit

--- lib/my-pipeline-lambda-stack.ts
+++ lib/my-pipeline-lambda-stack.ts
@@ -9,7 +9,7 @@ export class MyLambdaStack extends cdk.Stack {
       new Function(this, 'LambdaFunction', {
         runtime: Runtime.NODEJS_12_X,
         handler: 'index.handler',
-        code: new InlineCode('exports.handler = _ => "Hello, CDK";')
+        code: new InlineCode('exports.handler = _ => "My, CDK";')
       });
     }
 }
